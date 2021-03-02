1efd1d2e801620b471c5aa65b03980a90d479345
SHANTI LAL <shandude786@users.noreply.github.com>
2021-03-02 19:13:22 +0530

    second commit
    for component ->GIForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-//import GIForm from "./Forms/GIForm";
+import GIForm from "./Forms/GIForm";
 //import CrudFormReact from "./Forms/CrudFormReact";
 
-import FetchApi from "./components/FetchApi";
+//import FetchApi from "./components/FetchApi";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <FetchApi />
+        <GIForm />
       </div>
     );
   }
